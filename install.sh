9f76bc76d0ce0b31f116adfa0456020b75d15501
zooy0rk <zooy0rk@zooy0rk.net>
2014-11-18 16:06:54 -0500

    - Adding install.sh script

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Pull in submodules
+
+git submodule init 
+git submodule update
+
+# Loop through all dotfiles, make backups and create symlinks
+
+for dotfile in $(ls -A | grep '^\.')
+do
+	[[ -f ~/${dotfile} ]] && mv ~/${dotfile} ~/${dotfile}.orig
+
+	ln -s $(pwd)/${dotfile} ~/${dotfile}
+done
+
+# Just because
+
+. ~/.bashrc
